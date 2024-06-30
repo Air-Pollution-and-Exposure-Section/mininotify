8a3f44df51e328183791af4c03c6865b07b09db5
Jonathan Levine <jonathanaaron.levine@gmail.com>
2024-06-30 08:58:05 -0400

    fetching emails from database works!

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import requests
 import json
+from typing import List, Union, Tuple
 
-# api key
+# CONFIGS
 from config import API_KEY as api_key
-from config import EMAIL_ADDRESS as email_address
 from config import TEMPLATE_ID as template_id
 
 from sqlalchemy import create_engine
@@ -38,8 +38,35 @@ class Handler():
         else:
             print("No session to close")
 
-if __name__=="__main__":
 
+def send_email(email:str)->Tuple[str, json]:
+  # Define the headers and data
+  headers = {
+      'Authorization': f'ApiKey-v1 {api_key}',
+      'Content-Type': 'application/json',
+      'base_url': 'https://notification.canada.ca'
+  }
+
+  data = {
+      'email_address': email,
+      'template_id': template_id
+  }
+
+  # Make the POST request
+  response = requests.post(
+      'https://api.notification.canada.ca/v2/notifications/email',
+      headers=headers,
+      data=json.dumps(data)
+  )
+
+  # Print the response
+  print("status code: ", response.status_code)
+  print("response: ", response.json())
+  return [response.status_code, response.json()]
+
+
+if __name__=="__main__":
+  # DB Handler configs
   from config import DBNAME as dbname
   from config import USER as user
   from config import PASSWORD as password
@@ -56,25 +83,4 @@ if __name__=="__main__":
   for email in email_query:
     print(email)
 
-  # # Define the headers and data
-  # headers = {
-  #     'Authorization': f'ApiKey-v1 {api_key}',
-  #     'Content-Type': 'application/json',
-  #     'base_url': 'https://notification.canada.ca'
-  # }
-
-  # data = {
-  #     'email_address': email_address,
-  #     'template_id': template_id
-  # }
-
-  # # Make the POST request
-  # response = requests.post(
-  #     'https://api.notification.canada.ca/v2/notifications/email',
-  #     headers=headers,
-  #     data=json.dumps(data)
-  # )
-
-  # # Print the response
-  # print("status code: ", response.status_code)
-  # print("response: ", response.json())
+  print(send_email(email=email))
